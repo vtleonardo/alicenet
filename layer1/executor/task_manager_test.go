537feac7c39f53c3273e128dafc8f5ef1bc8c8a1
stuckDaemon <gabriele.bonadiman@gmail.com>
2022-06-28 19:52:11 +0200

    more unit tests for taskmanageer

--- layer1/executor/task_manager_test.go
+++ layer1/executor/task_manager_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"math/big"
 	"testing"
-	"time"
 )
 
 func getTaskManager(t *testing.T) (*TasksManager, *mocks.MockClient, *db.Database, *taskResponseChan, *mocks.MockWatcher) {
@@ -79,7 +78,7 @@ func Test_TaskManager_HappyPath(t *testing.T) {
 	assert.Emptyf(t, manager.Transactions, "Expected transactions to be empty")
 }
 
-func Test_TaskManager_TaskErrorRecoverable_ContextTimeout(t *testing.T) {
+func Test_TaskManager_TaskErrorRecoverable(t *testing.T) {
 	manager, client, db, taskRespChan, txWatcher := getTaskManager(t)
 	defer taskRespChan.close()
 
@@ -98,14 +97,55 @@ func Test_TaskManager_TaskErrorRecoverable_ContextTimeout(t *testing.T) {
 
 	taskErr := tasks.NewTaskErr("Recoverable error", true)
 	task := mocks.NewMockTask()
-	task.PrepareFunc.SetDefaultReturn(taskErr)
+	task.PrepareFunc.PushReturn(taskErr)
+	task.PrepareFunc.PushReturn(nil)
+	txn := types.NewTx(&types.LegacyTx{
+		Nonce:    1,
+		Value:    big.NewInt(1),
+		Gas:      1,
+		GasPrice: big.NewInt(1),
+		Data:     []byte{52, 66, 175, 92},
+	})
+	task.ExecuteFunc.SetDefaultReturn(txn, nil)
+	task.ShouldExecuteFunc.SetDefaultReturn(true, nil)
+	task.GetLoggerFunc.SetDefaultReturn(manager.logger)
 
-	mainCtx, _ := context.WithTimeout(context.Background(), 2*time.Second)
-	manager.ManageTask(mainCtx, task, "context timeout", "123", db, manager.logger, client, taskRespChan)
+	mainCtx := context.Background()
+	manager.ManageTask(mainCtx, task, "recoverable error", "123", db, manager.logger, client, taskRespChan)
+
+	mockrequire.CalledN(t, task.PrepareFunc, 2)
+	mockrequire.CalledOnce(t, task.ExecuteFunc)
+	mockrequire.CalledOnceWith(t, task.FinishFunc, mockrequire.Values(nil))
+	assert.Emptyf(t, manager.Transactions, "Expected transactions to be empty")
+}
+
+func Test_TaskManager_UnrecoverableError(t *testing.T) {
+	manager, client, db, taskRespChan, txWatcher := getTaskManager(t)
+	defer taskRespChan.close()
+
+	receipt := &types.Receipt{
+		Status:      types.ReceiptStatusSuccessful,
+		BlockNumber: big.NewInt(20),
+	}
+
+	receiptResponse := mocks.NewMockReceiptResponse()
+	receiptResponse.IsReadyFunc.SetDefaultReturn(true)
+	receiptResponse.GetReceiptBlockingFunc.SetDefaultReturn(receipt, nil)
+
+	txWatcher.SubscribeFunc.SetDefaultReturn(receiptResponse, nil)
+
+	client.GetTransactionReceiptFunc.SetDefaultReturn(receipt, nil)
+
+	task := mocks.NewMockTask()
+	taskErr := tasks.NewTaskErr("Unrecoverable error", false)
+
+	task.PrepareFunc.SetDefaultReturn(taskErr)
+	mainCtx := context.Background()
+	manager.ManageTask(mainCtx, task, "happyPath", "123", db, manager.logger, client, taskRespChan)
 
 	mockrequire.CalledOnce(t, task.PrepareFunc)
 	mockrequire.NotCalled(t, task.ExecuteFunc)
-	mockrequire.CalledOnce(t, task.FinishFunc)
+	mockrequire.CalledOnceWith(t, task.FinishFunc, mockrequire.Values(taskErr))
 	assert.Emptyf(t, manager.Transactions, "Expected transactions to be empty")
 }
 
