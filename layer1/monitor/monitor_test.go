514ed813495c5cd90438ab0cd68a94e42d461843
Egor Kostetskiy <kosegor@gmail.com>
2022-07-08 12:55:20 -0300

    monitor tests fixed

--- layer1/monitor/monitor_test.go
+++ layer1/monitor/monitor_test.go
@@ -4,7 +4,9 @@ package monitor
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/alicenet/alicenet/bridge/bindings"
+	"github.com/alicenet/alicenet/consensus/objs"
 	"github.com/alicenet/alicenet/constants"
 	"github.com/alicenet/alicenet/layer1/ethereum"
 	"github.com/alicenet/alicenet/layer1/executor"
@@ -64,6 +66,7 @@ func populateMonitor(monitorState *objects.MonitorState, EPOCH uint32) {
 
 func getMonitor(t *testing.T) (*monitor, *executor.TasksScheduler, chan tasks.TaskRequest, *mocks.MockClient) {
 	monDB := mocks.NewTestDB()
+	consDB := mocks.NewTestDB()
 	adminHandler := mocks.NewMockAdminHandler()
 	depositHandler := mocks.NewMockDepositHandler()
 	eth := mocks.NewMockClient()
@@ -73,7 +76,7 @@ func getMonitor(t *testing.T) (*monitor, *executor.TasksScheduler, chan tasks.Ta
 	contracts := mocks.NewMockContracts()
 	contracts.GetAllAddressesFunc.SetDefaultReturn([]common.Address{})
 
-	mon, err := NewMonitor(monDB, monDB, adminHandler, depositHandler, eth, contracts, 2*time.Second, 100, tasksReqChan)
+	mon, err := NewMonitor(consDB, monDB, adminHandler, depositHandler, eth, contracts, 2*time.Second, 100, tasksReqChan)
 	assert.Nil(t, err)
 	EPOCH := uint32(1)
 	populateMonitor(mon.State, EPOCH)
@@ -108,28 +111,35 @@ func TestMonitorPersist(t *testing.T) {
 func TestProcessEvents(t *testing.T) {
 	mon, tasksScheduler, tasksReqChan, eth := getMonitor(t)
 
-	defer close(tasksReqChan)
 	err := tasksScheduler.Start()
 	assert.Nil(t, err)
-	defer tasksScheduler.Close()
 
 	ethDkgMock := mocks.NewMockIETHDKG()
 	event := &bindings.ETHDKGRegistrationOpened{
 		StartBlock:         big.NewInt(10),
-		NumberValidators:   big.NewInt(1),
+		NumberValidators:   big.NewInt(4),
 		Nonce:              big.NewInt(1),
-		PhaseLength:        big.NewInt(1),
-		ConfirmationLength: big.NewInt(1),
+		PhaseLength:        big.NewInt(40),
+		ConfirmationLength: big.NewInt(10),
 		Raw:                types.Log{},
 	}
 	ethDkgMock.ParseRegistrationOpenedFunc.SetDefaultReturn(event, nil)
 	ethereum.SetEthDkg(ethDkgMock)
 
-	account := accounts.Account{Address: common.Address{1, 2, 3, 4}}
+	addr := common.Address{}
+	addr.SetBytes([]byte("546F99F244b7B58B855330AE0E2BC1b30b41302F"))
+	account := accounts.Account{
+		Address: addr,
+		URL: accounts.URL{
+			Scheme: "http",
+			Path:   "",
+		},
+	}
 	mon.State.PotentialValidators[account.Address] = objects.PotentialValidator{
 		Account: account.Address,
 	}
 	eth.EndpointInSyncFunc.SetDefaultReturn(true, 4, nil)
+	eth.EndpointInSyncFunc.PushReturn(false, 0, errors.New("network failure"))
 	eth.GetFinalizedHeightFunc.SetDefaultReturn(1, nil)
 	eth.GetDefaultAccountFunc.SetDefaultReturn(account)
 
@@ -141,15 +151,39 @@ func TestProcessEvents(t *testing.T) {
 	eth.GetEventsFunc.SetDefaultReturn(nil, nil)
 	eth.GetEventsFunc.PushReturn(logs, nil)
 
-	<-time.After(5 * time.Second)
 	err = mon.Start()
 	assert.Nil(t, err)
-	defer mon.Close()
 
 	<-time.After(5 * time.Second)
+
 	assert.Equal(t, 2, len(tasksScheduler.Schedule))
 
 	dkgState, err := state.GetDkgState(mon.db)
 	assert.Nil(t, err)
 	assert.NotNil(t, dkgState)
+
+	mon.Close()
+	tasksScheduler.Close()
+	close(tasksReqChan)
+}
+
+func TestPersistSnapshot(t *testing.T) {
+	mon, tasksScheduler, tasksReqChan, eth := getMonitor(t)
+	eth.GetFinalizedHeightFunc.SetDefaultReturn(1, nil)
+
+	defer close(tasksReqChan)
+	err := tasksScheduler.Start()
+	assert.Nil(t, err)
+	defer tasksScheduler.Close()
+
+	bh := &objs.BlockHeader{
+		BClaims: &objs.BClaims{
+			Height: 1,
+		},
+	}
+	err = PersistSnapshot(eth, bh, tasksReqChan, mon.db)
+
+	<-time.After(constants.TaskSchedulerProcessingTime)
+
+	assert.Equal(t, 1, len(tasksScheduler.Schedule))
 }
